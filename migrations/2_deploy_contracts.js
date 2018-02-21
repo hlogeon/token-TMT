437afe6ea7506374a15fe4fd25a51c833a751312
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-02-22 02:07:36 +0800

    Renaming

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,20 @@
 var SafeMath = artifacts.require('./SafeMath.sol');
-var JincorToken = artifacts.require("./JincorToken.sol");
-var JincorTokenPreSale = artifacts.require("./JincorTokenPreSale.sol");
+var TicketManiaToken = artifacts.require("./TicketManiaToken.sol");
+var TicketManiaTokenPreSale = artifacts.require("./TicketManiaTokenPreSale.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SafeMath);
-  deployer.link(SafeMath, JincorToken);
-  deployer.link(SafeMath, JincorTokenPreSale);
-  deployer.deploy(JincorToken).then(function() {
+  deployer.link(SafeMath, TicketManiaToken);
+  deployer.link(SafeMath, TicketManiaTokenPreSale);
+  deployer.deploy(TicketManiaToken).then(function() {
     const hardCap = 350000; //in USD
     const softCap = 150000; //in USD
-    const token = JincorToken.address;
+    const token = TicketManiaToken.address;
     const totalTokens = 1400000; //NOT in wei, converted by contract
     const limit = 50000; //in USD
     const beneficiary = web3.eth.accounts[0];
     const startBlock = web3.eth.blockNumber;
     const endBlock = web3.eth.blockNumber + 100;
-    deployer.deploy(JincorTokenPreSale, hardCap, softCap, token, beneficiary, totalTokens, 255, limit, startBlock, endBlock);
+    deployer.deploy(TicketManiaTokenPreSale, hardCap, softCap, token, beneficiary, totalTokens, 255, limit, startBlock, endBlock);
   });
 };
